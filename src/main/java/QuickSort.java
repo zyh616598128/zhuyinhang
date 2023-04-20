9ee5618597ac0e21f56058319334e1679a147ffc
Zhuyinhang <616598128@qq.com>
2023-04-20 11:31:11 +0800

    初始化仓库

---
+++ src/main/java/QuickSort.java
@@ -0,0 +1,38 @@
+public class QuickSort {
+    public static void sort(int[] arr, int left, int right) {
+        if (left < right) {
+            int pivot = partition(arr, left, right);
+            sort(arr, left, pivot - 1);
+            sort(arr, pivot + 1, right);
+        }
+    }
+
+    private static int partition(int[] arr, int left, int right) {
+        int pivot = arr[right];
+        int i = left - 1;
+        for (int j = left; j < right; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, right);
+        return i + 1;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 7, 8, 9, 1, 5};
+        int n = arr.length;
+        sort(arr, 0, n - 1);
+        System.out.println("Sorted array:");
+        for (int i = 0; i < n; ++i) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
